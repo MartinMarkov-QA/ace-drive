a534fe7c2581b03e26f0076355ca31dcc47f8248
Martin Markov <martin.markov@weareaccess.co.uk>
2021-12-02 12:38:57 +0000

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let homeSection = document.querySelector('.home');
+// let navbarToggler = document.querySelector('.navbar-toggler');
+
+// navbarToggler.addEventListener('click', () => {
+//     if (navbarToggler.classList.contains('collapsed')) {
+//         // homeSection.style.height = '100vh'
+//         console.log('closed');
+//     }   else {
+//         // homeSection.style.height = '130vh'
+//         console.log('open');
+//     }
+// })
+
+// console.log(homeSection, navbarToggler);
